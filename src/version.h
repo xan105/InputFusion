e3e6e7f97f8e7b3927a09a4649c2ec3bef9ef956
Anthony Beaumont <dev.xan105@outlook.be>
2026-06-12 09:58:50 +0700

    v0.4.0.1

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 	#define VERSION_H
 
-	#define VER_FILEVERSION             0,4,0,0
-	#define VER_FILEVERSION_STR         "0.4.0.0\0"
+	#define VER_FILEVERSION             0,4,0,1
+	#define VER_FILEVERSION_STR         "0.4.0.1\0"
 
-	#define VER_PRODUCTVERSION          0,4,0,0
-	#define VER_PRODUCTVERSION_STR      "0.4.0.0\0"
+	#define VER_PRODUCTVERSION          0,4,0,1
+	#define VER_PRODUCTVERSION_STR      "0.4.0.1\0"
 
 	#define VER_COMPANYNAME_STR         "Anthony Beaumont (Xan)"
 	#define VER_FILEDESCRIPTION_STR     "InputFusion"
